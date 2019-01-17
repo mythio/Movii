49a950af9c0968490cd68742298ab0c709207d25
mythio <mythio.2909@gmail.com>
2019-01-17 17:04:34 +0530

    Changed the model

--- app/src/main/java/com/mythio/movii/Model/Movie.java
+++ app/src/main/java/com/mythio/movii/Model/Movie.java
@@ -1,50 +1,88 @@
 package com.mythio.movii.Model;
 
+import java.util.List;
+
 public class Movie {
     private String imdb;
     private String poster_path;
-    private String original_title;
+    private String title;
     private String imdb_id;
     private String release_date;
     private String overview;
-    private String production;
-    private String backdrop_path;
+    private String length;
+    private List<String> starCast;
+    private Double ratings;
 
-    public Movie(String original_title, String poster_path, String backdrop_path) {
-        this.original_title = original_title;
-        this.poster_path = poster_path;
-        this.backdrop_path = backdrop_path;
-    }
 
     public String getImdb() {
         return imdb;
+    }
+
+    public void setImdb(String imdb) {
+        this.imdb = imdb;
     }
 
     public String getPoster_path() {
         return poster_path;
     }
 
-    public String getOriginal_title() {
-        return original_title;
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getImdb_id() {
         return imdb_id;
     }
 
+    public void setImdb_id(String imdb_id) {
+        this.imdb_id = imdb_id;
+    }
+
     public String getRelease_date() {
         return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
     }
 
     public String getOverview() {
         return overview;
     }
 
-    public String getProduction() {
-        return production;
+    public void setOverview(String overview) {
+        this.overview = overview;
     }
 
-    public String getBackdrop_path() {
-        return backdrop_path;
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public List<String> getStarCast() {
+        return starCast;
+    }
+
+    public void setStarCast(List<String> starCast) {
+        this.starCast = starCast;
+    }
+
+    public Double getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(Double ratings) {
+        this.ratings = ratings;
     }
 }
