550e6e84d584ca507cd4011cb0deb1b5f3ced28a
mythio <mythio.2909@gmail.com>
2019-01-22 17:13:33 +0530

    Added custom tab layout

--- app/src/main/java/com/mythio/movii/Fragment/MoviesFragment.java
+++ app/src/main/java/com/mythio/movii/Fragment/MoviesFragment.java
@@ -3,17 +3,72 @@ package com.mythio.movii.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mythio.movii.R;
+import com.mythio.movii.temp.Fragment1;
+import com.mythio.movii.temp.Fragment2;
+import com.mythio.movii.temp.Fragment3;
+import com.mythio.movii.temp.Fragment5;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MoviesFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        View view = inflater.inflate(R.layout.fragment_movies, container, false);
+        ViewPager viewPager = view.findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+        TabLayout tabs = view.findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        tabs.setSelectedTabIndicator(R.color.pixel_blue);
+        return view;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getChildFragmentManager());
+        adapter.addFragment(new Fragment1(), "Popular");
+        adapter.addFragment(new Fragment2(), "Latest");
+        adapter.addFragment(new Fragment3(), "Trending");
+        adapter.addFragment(new Fragment5(), "Highest Rated");
+        viewPager.setAdapter(adapter);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
---
+++ app/src/main/java/com/mythio/movii/temp/Fragment4.java
@@ -0,0 +1,26 @@
+package com.mythio.movii.temp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mythio.movii.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fragment4 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_series, container, false);
+    }
+}
