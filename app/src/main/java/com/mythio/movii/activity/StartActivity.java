be1c6932c668b4d4a8d2d5e042c23d71323c1464
mythio <mythio.2909@gmail.com>
2019-03-02 14:29:31 +0530

    Added bubble bottom navigation bar

--- app/src/main/java/com/mythio/movii/activity/StartActivity.java
+++ app/src/main/java/com/mythio/movii/activity/StartActivity.java
@@ -8,11 +8,17 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.gauravk.bubblenavigation.BubbleNavigationConstraintView;
+import com.gauravk.bubblenavigation.listener.BubbleNavigationChangeListener;
 import com.mythio.movii.R;
 import com.mythio.movii.fragment.MoviesFragment;
 import com.mythio.movii.fragment.ProfileFragment;
 import com.mythio.movii.fragment.TvShowsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -21,36 +27,29 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        BubbleNavigationConstraintView navBar = findViewById(R.id.bottom_navigation);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MoviesFragment()).commit();
-    }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                @Override
-                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                    Fragment fragment = null;
-
-                    switch (menuItem.getItemId()) {
-                        case R.id.navigation_movies:
-                            fragment = new MoviesFragment();
-                            break;
-                        case R.id.navigation_tv:
-                            fragment = new TvShowsFragment();
-                            menuItem.setIcon(R.drawable.ic_search);
-                            break;
-                        case R.id.navigation_profiles:
-                            fragment = new ProfileFragment();
-                            break;
-                    }
-
-                    assert fragment != null;
-                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                    ft.setAllowOptimization(true);
-                    ft.replace(R.id.fragment_container, fragment).commit();
-                    return true;
+        navBar.setNavigationChangeListener(new BubbleNavigationChangeListener() {
+            @Override
+            public void onNavigationChanged(View view, int position) {
+                Fragment fragment = null;
+                switch (position) {
+                    case 0:
+                        fragment = new MoviesFragment();
+                        break;
+                    case 1:
+                        fragment = new TvShowsFragment();
+                        break;
+                    case 2:
+                        fragment = new ProfileFragment();
+                        break;
                 }
-            };
+                if (fragment != null) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                }
+            }
+        });
+    }
 }
