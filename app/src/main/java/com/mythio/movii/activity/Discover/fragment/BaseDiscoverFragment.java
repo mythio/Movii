5afbed59caa0c069034d3f5bc4b5d842d0216701
mythio <mythio.2909@gmail.com>
2019-05-29 04:34:46 +0530

    [refactor]: Remove placeholders

--- app/src/main/java/com/mythio/movii/activity/Discover/fragment/BaseDiscoverFragment.java
+++ app/src/main/java/com/mythio/movii/activity/Discover/fragment/BaseDiscoverFragment.java
@@ -9,17 +9,17 @@ import androidx.fragment.app.Fragment;
 
 import com.mythio.movii.activity.Discover.fragment.contract.DiscoverFragmentNavigation;
 
+import org.jetbrains.annotations.NotNull;
+
 public abstract class BaseDiscoverFragment extends Fragment implements DiscoverFragmentNavigation.View {
 
-    protected View rootView;
     protected DiscoverFragmentNavigation.Presenter presenter;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        rootView = inflater.inflate(getLayout(), container, false);
-        return rootView;
+        return inflater.inflate(getLayout(), container, false);
     }
 
     protected abstract int getLayout();
