322d1588fb5920ea4b0cd6491ce1e26aa3c1f2a6
mythio <mythio.2909@gmail.com>
2019-06-15 18:15:18 +0530

    [chore]: Refactor Api interface

--- app/src/main/java/com/mythio/movii/network/ApiTmdb.java
+++ app/src/main/java/com/mythio/movii/network/ApiTmdb.java
@@ -18,28 +18,49 @@ public interface ApiTmdb {
 
     @NonNull
     @GET("movie/popular")
-    Single<MovieResponse> getPopularMovies(@Query("api_key") String apiKey);
+    Single<MovieResponse> getPopularMovies(
+            @Query("api_key") String apiKey
+    );
 
     @NonNull
     @GET("movie/{movie_id}")
-    Single<Movie> getMovieDetail(@Path("movie_id") Integer movieId, @Query("api_key") String apiKey, @Query("append_to_response") String parameter);
+    Single<Movie> getMovieDetail(
+            @Path("movie_id") Integer movieId,
+            @Query("api_key") String apiKey,
+            @Query("append_to_response") String parameter
+    );
 
     @NonNull
     @GET("search/movie")
-    Observable<MovieResponse> getMovieSearchResults(@Query("api_key") String apiKey, @Query("query") String query);
+    Observable<MovieResponse> getMovieSearchResults(
+            @Query("api_key") String apiKey,
+            @Query("query") String query
+    );
 
     @NonNull
     @GET("tv/popular")
-    Single<TvShowResponse> getPopularTvShows(@Query("api_key") String apiKey);
+    Single<TvShowResponse> getPopularTvShows(
+            @Query("api_key") String apiKey
+    );
 
     @NonNull
     @GET("tv/{tv_show_id}")
-    Observable<TvShow> getTvShowDetail(@Path("tv_show_id") int tvShowId, @Query("api_key") String apiKey, @Query("append_to_response") String parameter);
+    Observable<TvShow> getTvShowDetail(
+            @Path("tv_show_id") int tvShowId,
+            @Query("api_key") String apiKey,
+            @Query("append_to_response") String parameter
+    );
 
     @NonNull
     @GET("search/tv")
-    Observable<TvShowResponse> getTvShowSearchResults(@Query("api_key") String apiKey, @Query("query") String query);
+    Observable<TvShowResponse> getTvShowSearchResults(
+            @Query("api_key") String apiKey,
+            @Query("query") String query
+    );
 
     @GET("person/{cast_id}/movie_credits")
-    Single<CastMovies> getCastMovies(@Path("cast_id") int id, @Query("api_key") String apiKey);
+    Single<CastMovies> getCastMovies(
+            @Path("cast_id") int id,
+            @Query("api_key") String apiKey
+    );
 }
--- app/src/main/java/com/mythio/movii/network/ApiVideoSpider.java
+++ app/src/main/java/com/mythio/movii/network/ApiVideoSpider.java
@@ -1,16 +1,20 @@
 package com.mythio.movii.network;
 
+import androidx.annotation.NonNull;
+
 import io.reactivex.Single;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiVideoSpider {
 
+    @NonNull
     @GET("/")
     Single<String> getIpAddress(
 
     );
 
+    @NonNull
     @GET("getticket.php")
     Single<String> getTicket(
             @Query("key") String key,
@@ -19,6 +23,7 @@ public interface ApiVideoSpider {
             @Query("ip") String ip
     );
 
+    @NonNull
     @GET("getvideo")
     Single<String> getLink(
             @Query("key") String key,
